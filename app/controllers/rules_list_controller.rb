79d2713518bd0598f15a6355f29f7a4f8a84333e
Laura Brooks <laura.brooks0@outlook.com>
2022-04-11 12:32:11 +0900

    rules list new and create coded

--- app/controllers/rules_list_controller.rb
+++ app/controllers/rules_list_controller.rb
@@ -1,2 +1,34 @@
 class RulesListController < ApplicationController
+  before_action :set_rules_list, only: [:show]
+
+  def new
+    @rules_list = RulesList.new
+  end
+
+  def create
+    @rules_list = RulesList.new(rules_list_params)
+    if @rules_list.save
+      redirect_to rules_list_path(@rules_list)
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  private
+  def set_rules_list
+    @rules_list = RulesList.find(params[:id])
+  end
+
+  def rules_list_params
+    params.require(:rules_list).permit(:name)
+  end
 end
