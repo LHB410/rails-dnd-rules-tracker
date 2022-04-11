118d7f221faab5aaef88e71c20b218bd8ba43513
Laura Brooks <laura.brooks0@outlook.com>
2022-04-11 12:20:12 +0900

    controllers created

---
+++ app/controllers/rules_list_controller.rb
@@ -0,0 +1,2 @@
+class RulesListController < ApplicationController
+end
---
+++ test/controllers/rules_list_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RulesListControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
